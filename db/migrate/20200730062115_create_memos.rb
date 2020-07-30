cfd92f31ac30381348ba2df3f425ce8c544feb70
vagrant <vagrant@localhost.localdomain>
2020-07-30 13:45:00 +0000

    [Finish]

---
+++ db/migrate/20200730062115_create_memos.rb
@@ -0,0 +1,11 @@
+class CreateMemos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :memos do |t|
+      t.string :title
+      t.string :string
+      t.string :comment
+
+      t.timestamps
+    end
+  end
+end
